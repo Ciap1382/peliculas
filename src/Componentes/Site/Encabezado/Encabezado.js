b018e68c618606258d9f364dbdb8c46983e365b9
CarlosArreolaLopez <carlos.arreola@hotmail.com>
2023-12-09 17:21:10 -0600

    ejemplo004

--- src/Componentes/Site/Encabezado/Encabezado.js
+++ src/Componentes/Site/Encabezado/Encabezado.js
@@ -4,7 +4,7 @@ const Encabezado=()=>{
    return(
     <>
     <div id="encabezado" className="colobase" >
-       <div id="logotipo">logotipo</div>
+       <div id="logotipo">logotipo peliculas</div>
        <div id="generos"><Selgeneros/></div>
        <div id="registro"> registro</div>
     </div>
