b4051002ca8659c9e39508c397d5a33864021fbc
Carlos Javier Aceves Perez <ciap1382@gmail.com>
2023-12-09 17:45:57 -0600

    subiendo cambios

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import './Estilos.css';
 import Encabezado from './Componentes/Site/Encabezado/Encabezado';
 import Piepagina from './Componentes/Site/Piepagina/Piepagina';
 import Contenido from './Componentes/Site/Contenido/Contenido';
-
 function App() {   
- 
   return (  
        <div className="App">
         <Encabezado/>
         <Contenido/>
         <Piepagina/>
-     
     </div>
   )}
 export default App;
